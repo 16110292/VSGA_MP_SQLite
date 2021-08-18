7fd57be2a150454ba694f0ced1b2b8c28a9bb2f0
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-18 23:42:26 +0700

    Part 5

--- app/src/main/java/com/example/vsga_mp_sqlite/LoginActivity.java
+++ app/src/main/java/com/example/vsga_mp_sqlite/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.vsga_mp_sqlite;
 
 import static android.text.Html.fromHtml;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,6 +34,40 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView tvCreateAccount = (TextView) findViewById(R.id.tv_create);
 
-        tvCreateAccount.setText(fromHtml( "I don't have account yet. " + "</font><"));
+        tvCreateAccount.setText(fromHtml( "I don't have account yet. " + "</font><font color='#3b5432'>create one<?font>"));
+
+        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_user.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+
+                Boolean res = dbHelper.checkUser(username, password);
+                if (res == true) {
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this MainActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    public static Spanned fromHtml(String html) {
+        Spanned result;
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
     }
 }
