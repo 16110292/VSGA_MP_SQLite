dc440e95e6166930549a32a673a067e0fdacc5eb
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-18 23:19:03 +0700

    Part 4

--- app/src/main/java/com/example/vsga_mp_sqlite/LoginActivity.java
+++ app/src/main/java/com/example/vsga_mp_sqlite/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.example.vsga_mp_sqlite;
 
+import static android.text.Html.fromHtml;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+        EditText et_user, et_password;
+        Button bt_login;
+        DBHelper dbHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_user = (EditText) findViewById(R.id.et_user_login);
+        et_password = (EditText) findViewById(R.id.et_pass_login);
+        bt_login = (Button) findViewById(R.id.bt_login);
+
+        dbHelper = new DBHelper(this);
+
+        TextView tvCreateAccount = (TextView) findViewById(R.id.tv_create);
+
+        tvCreateAccount.setText(fromHtml( "I don't have account yet. " + "</font><"));
     }
 }
--- app/src/main/java/com/example/vsga_mp_sqlite/MainActivity.java
+++ app/src/main/java/com/example/vsga_mp_sqlite/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.vsga_mp_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_lo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
